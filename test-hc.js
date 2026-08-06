5778c4c7b996cfa30b0365b8f38165c7bd4fd91f
Vishal <64590881+SA7ANI@users.noreply.github.com>
2026-08-06 17:20:54 +0530

    Delete test-hc.js

--- test-hc.js
+++
@@ -1,4 +0,0 @@
-const axios = require('axios');
-axios.head('https://hubcloud.ist/drive/bj2u5ss6yu51xzs')
-    .then(res => console.log('HEAD status:', res.status))
-    .catch(err => console.log('HEAD error:', err.response ? err.response.status : err.message));
